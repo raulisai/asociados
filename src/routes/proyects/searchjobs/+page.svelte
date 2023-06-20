2506d92877e7dd8d87a41b827c32e8ff004c7ab2
Davidcerv <davcertor@gmail.com>
2023-06-20 04:32:28 -0600

    ¨form¨

---
+++ src/routes/proyects/searchjobs/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+</script>
+
+<section class="row">
+  <div class="col-12">
+    <h3 class="ms-5">Crea tu cv</h3>
+
+    <div class="row mt-3">
+      <div class="col">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Nombre"
+          aria-label="Nombre"
+        />
+      </div>
+      <div class="col">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Apellido"
+          aria-label="Apellido"
+        />
+      </div>
+      <div class="col">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Edad"
+          aria-label="Edad"
+        />
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <input
+          type="correo"
+          class="form-control"
+          placeholder="correo"
+          aria-label="Nombre"
+        />
+      </div>
+      <div class="col-md-6">
+        <input
+          type="number"
+          class="form-control"
+          id="colFormLabel"
+          placeholder="telefono"
+        />
+      </div>
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="profesion"
+            aria-label="Nombre"
+          />
+        </div>
+        <div class="col-md-6">
+          <input
+            type="text"
+            class="form-control"
+            id="colFormLabel"
+            placeholder="especialidad"
+          />
+        </div>
+        <div class="col-md-12 mt-5">
+            <textarea class="responsive-textarea" placeholder="Descripcion"></textarea>
+          </div>
+      </div>
+
+     
+    </div>
+  </div>
+</section>
+
+
+<style>
+    .responsive-textarea {
+        width: 100%;
+        max-width: 1200px;
+        height: 200px;
+        resize: vertical;
+    }
+</style>
