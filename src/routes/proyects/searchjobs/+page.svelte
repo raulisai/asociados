5eac16971b7056749ca3b526173e7330f5c331af
raulisai <raulisai97@gmail.com>
2023-06-22 22:46:50 -0600

    login ver 0.2

--- src/routes/proyects/searchjobs/+page.svelte
+++ src/routes/proyects/searchjobs/+page.svelte
@@ -1,5 +1,26 @@
 <script>
-import Skill from './skill.svelte'
+    import Form from "./Form.svelte";
+    import { onMount } from "svelte";
+
+    let SaveCv = false;
+    let activeFormcv=false
+    let button1Text = "Crear Cv";
+
+    if (SaveCv) {
+        button1Text = "Actualizar Cv";
+    }
+
+    function handlerCv() {
+
+        console.log("hello")
+        activeFormcv=!activeFormcv
+        button1Text = "Ocultar seccion Cv";
+
+        if (activeFormcv == false) {
+        button1Text = "Crear Cv";
+    }
+
+    }
 
     let data = [
         {
@@ -27,9 +48,48 @@ import Skill from './skill.svelte'
     });
 </script>
 
+<section class="row">
+    <h3>Creador de Cv Automatizado</h3>
 
-<section>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Puesto</th>
+                <th>Empresa</th>
+                <th>CV usado</th>
+                <th>Estatus</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each data as item}
+                <tr>
+                    <td>{item.puesto}</td>
+                    <td>{item.empresa}</td>
+                    <td>{item.cv}</td>
+                    <td>{item.estatus}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-6">
+                <button class="btn" on:click={handlerCv}>{button1Text}</button>
+            </div>
+        </div>
+    </div>
 
-    <Skill/>
+    <div class="col-12">
 
+{#if activeFormcv}
+<Form/>
+{/if}
+
+    </div>
 </section>
+
+<style>
+    .table {
+        margin-bottom: 0; /* Para evitar espacios innecesarios en la parte inferior de la tabla */
+    }
+</style>
